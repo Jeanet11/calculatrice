700dbf4f6aef194a83079c3d97bc93b2e8ff44c4
Jeanne <jeanne.tiramani@hotmail.fr>
2017-07-04 11:48:45 +0200

    fichier calculatrice

---
+++ calc.js
@@ -0,0 +1,7 @@
+var nombre1 = Number(prompt("Entrez le nombre 1"));
+
+var nombre2 = Number(prompt("Entrez le nombre 2")) ;
+
+var somme = nombre1 + nombre2;
+var multiplication = nombre1 * nombre2;
+alert ("Le résultat de la somme est : " + somme  + "\nLe résultat de la multiplication est : " + multiplication);
