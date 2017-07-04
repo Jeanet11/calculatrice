4e4117b7ddcedabf65be5de12f8bbfb4e3f14767
Jeanne <jeanne.tiramani@hotmail.fr>
2017-07-04 15:48:08 +0200

    rajout de fonction

--- calc.js
+++ calc.js
@@ -2,6 +2,21 @@ var nombre1 = Number(prompt("Entrez le nombre 1"));
 
 var nombre2 = Number(prompt("Entrez le nombre 2")) ;
 
-var somme = nombre1 + nombre2;
+/*var somme = nombre1 + nombre2;
 var multiplication = nombre1 * nombre2;
 alert ("Le résultat de la somme est : " + somme  + "\nLe résultat de la multiplication est : " + multiplication);
+*/
+
+function somme (nombre1, nombre2) {
+	return nombre1 + nombre2;
+}
+
+function produit (nombre1, nombre2) {
+	return nombre1 * nombre2;
+}
+
+
+
+var monTitre = document.querySelector("p");
+monTitre.textContent = "La somme est " + somme(nombre1, nombre2) + " et le produit est " + produit(nombre1, nombre2);
+/*alert ("La somme est : " + somme(nombre1, nombre2) + "\nLe produit est : " + produit(nombre1, nombre2));*/
